7c2f3257adbcf30b5b6525230ed911e2313841ed
BroGetTheCam <96469630+BroGetTheCam@users.noreply.github.com>
2024-09-21 21:43:33 -0400

    Integrating Custom User Model

--- urls.py
+++ urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-from .import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('', views.getRoutes),
-    path('rooms/', views.getRooms),
-    path('rooms/<str:pk>/', views.getRoom),
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+    path('api/', include('base.api.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
